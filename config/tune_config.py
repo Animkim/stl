80660a3433acd4e9b62f1dad8034136f768118a3
Animkim <aminkims@gmail.com>
2018-06-18 16:12:35 +0700

    small settings.py path fix

--- config/tune_config.py
+++ config/tune_config.py
@@ -4,23 +4,25 @@ import os
 import sys
 import argparse
 
+from django.conf import settings
 from django.core.management.utils import get_random_secret_key
 
 
 def patch_settings():
-    if not os.path.exists('../settings.py'):
+    path = os.path.join(settings.DJANGO_ROOT, 'settings.py')
+    if not os.path.exists(path):
         sys.stdout.write(
             'Error not found file settings.py'
-            'check its availability: {0}'.format('../settings.py')
+            'check its availability: {0}'.format(path)
         )
         return sys.exit(1)
 
-    with open('../settings.py', 'r') as st_original:
+    with open(path, 'r') as st_original:
         original = st_original.read()
 
     original = original.replace('SECRET_KEY = \'\'', 'SECRET_KEY = \'{0}\''.format(get_random_secret_key()))
 
-    with open('../settings.py', 'w') as st_new:
+    with open(path, 'w') as st_new:
         st_new.write(original)
 
 
