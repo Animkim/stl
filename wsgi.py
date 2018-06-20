dd9ed44f412e56056a17942744466dfabac83634
Animkim <aminkims@gmail.com>
2018-06-20 15:04:14 +0700

    uwsgi

--- wsgi.py
+++ wsgi.py
@@ -6,11 +6,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
-import sys
-print(sys.path)
 application = get_wsgi_application()
