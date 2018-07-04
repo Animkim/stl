34e41958de420f83c49737c96291dce37a931f4e
Animkim <aminkims@gmail.com>
2018-07-04 14:24:32 +0700

    sitemap fixes

--- urls.py
+++ urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.contrib.sitemaps.views import sitemap
 
-from stl.sitemap import AdSitemap
+from stl.sitemap import AdSitemap, PlaceSitemap
 from stl.main import views
 
 urlpatterns = [
     path('', views.main_page),
     path('robots.txt/', views.robots),
-    path('sitemap.xml/', sitemap, {'sitemaps': {'ad': AdSitemap}}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml/', sitemap, {'sitemaps': {'ad': AdSitemap, 'place': 'PlaceSitemap'}},
+         name='django.contrib.sitemaps.views.sitemap'),
     path('{ad}'.format(ad=settings.AD_PATH), views.ad_page, name='ad'),
     path('{location}'.format(location=settings.LOCATION_PATH), views.location_page),
 ]
