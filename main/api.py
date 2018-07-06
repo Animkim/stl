985648479ecbc3cc7cb42ebb4d2c4f85c29558da
Animkim <aminkims@gmail.com>
2018-07-06 15:02:13 +0700

    change structure data

--- main/api.py
+++ main/api.py
@@ -74,7 +74,7 @@ class AdCreator(object):
         if not self.data.get('object_type_id'):
             return None
         
-        self.data.update(self.raw_data['satellite'])
+        self.data.update(self.raw_data['snippet'])
         ad = Ad.objects.create(**self.data)
         ad.photos.set(self.get_photos())
         ad.places.set(Place.objects.filter(pk__in=self.raw_data.get('places', [])))
