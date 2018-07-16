c903f1510228f072c3bada5e7191167caff66f0c
Animkim <aminkims@gmail.com>
2018-07-16 18:34:46 +0700

    parse ads fixes

--- main/api.py
+++ main/api.py
@@ -64,7 +64,7 @@ class TranioApi(object):
                 clean_data['object_type'] = ObjectType.objects.filter(letter_id=data['object_type']).first()
                 clean_data['place'] = Place.objects.filter(pk=data['place']).first()
 
-                ad = Ad(**data)
+                ad = Ad(**clean_data)
                 ad.save()
                 existing_photos = AdPhoto.objects.filter(photo__in=data['photos']).values_list('pk', flat=True)
                 ad.photos.set([AdPhoto.objects.create(id=pk) for pk in data['photos'] if pk not in existing_photos])
