5f8c40c4c266197d74d383c680df2d56d53b1598
Animkim <aminkims@gmail.com>
2018-07-16 16:37:09 +0700

    fix

--- settings.py
+++ settings.py
@@ -23,7 +23,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'urls'
@@ -72,11 +71,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.contenttypes',
     'django.contrib.messages',
-    'django.contrib.admin',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-    'django.contrib.flatpages',
     'compressor',
     'stl.main',
 ]
