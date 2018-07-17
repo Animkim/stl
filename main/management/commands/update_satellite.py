dbd33f1145fdb89c4162ea96c3e4ee53d645da7b
Animkim <aminkims@gmail.com>
2018-07-17 17:57:27 +0700

    commands change

--- main/management/commands/update_satellite.py
+++ main/management/commands/update_satellite.py
@@ -5,8 +5,12 @@ from main.api import TranioApi
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('email', nargs='+', type=str, help='email')
-        parser.add_argument('--date_range', dest='date_range', type=str, help='dd.mm.yyyy-dd.mm.yyyy')
+        parser.add_argument('--ads', help='update Ad model', action='store_true')
+        parser.add_argument('--types', help='update ObjectType model', action='store_true')
+        parser.add_argument('--places', help='update Place model', action='store_true')
+        parser.add_argument('--pages', help='update StaticPage model', action='store_true')
+        parser.add_argument('--meta', help='update MetaData model', action='store_true')
+        parser.add_argument('--all', help='update all models', action='store_true')
 
     def handle(self, *args, **options):
         TranioApi().process(args)
