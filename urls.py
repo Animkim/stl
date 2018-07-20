34977a063a15909d0fed6e1bb303dd6ec0543302
Animkim <aminkims@gmail.com>
2018-07-20 17:20:15 +0700

    urls fix

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from stl.main import views
 
 urlpatterns = [
     path('robots.txt/', views.robots),
-    path('', views.route),
+    re_path('/$', views.route),
 ]
